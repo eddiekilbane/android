8d744d55481ff8ca9f37b8b1c136547b2a37ff59
Drew Carey Buglione <drew@dangerousthings.com>
2015-04-22 10:19:06 -0400

    Overhaul ControlActivity

--- src/com/switcherooboard/android/ControlActivity.java
+++ src/com/switcherooboard/android/ControlActivity.java
@@ -1,121 +1,75 @@
 package com.switcherooboard.android;
 
 import android.app.Activity;
-import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothGatt;
-import android.bluetooth.BluetoothGattCallback;
-import android.bluetooth.BluetoothGattCharacteristic;
-import android.bluetooth.BluetoothGattService;
-import android.bluetooth.BluetoothProfile;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 
-import java.util.concurrent.Semaphore;
-import java.util.UUID;
+public class ControlActivity extends Activity implements ISwitcherooCallback {
 
-public class ControlActivity extends Activity {
+    private static final String TAG = "ControlActivity";
 
-    private static final String TAG = "Switcheroo.ControlActivity";
+    private ISwitcheroo mSwitcheroo;
 
-    static final UUID SERVICE_UUID = UUID.fromString("00000015-9d7a-4919-b570-3bb24a4bf68e");
-    static final UUID CHARACTERISTIC_21_UUID = UUID.fromString("00000021-9d7a-4919-b570-3bb24a4bf68e");
-    static final UUID CHARACTERISTIC_22_UUID = UUID.fromString("00000022-9d7a-4919-b570-3bb24a4bf68e");
-    static final UUID CHARACTERISTIC_23_UUID = UUID.fromString("00000023-9d7a-4919-b570-3bb24a4bf68e");
-    static final UUID CHARACTERISTIC_24_UUID = UUID.fromString("00000024-9d7a-4919-b570-3bb24a4bf68e");
+    public void onSwitchClick(View view) {
+        SwitchButton button = (SwitchButton) view;
 
-    BluetoothDevice mBluetoothDevice;
-    BluetoothGatt mBluetoothGatt;
-    BluetoothGattService mBluetoothGattService;
+        final Integer index;
 
-    final Semaphore mSemaphore = new Semaphore(0);
+        if (button.getId() == R.id.switch_1) {
+            index = 0;
+        } else if (button.getId() == R.id.switch_2) {
+            index = 1;
+        } else if (button.getId() == R.id.switch_3) {
+            index = 2;
+        } else if (button.getId() == R.id.switch_4) {
+            index = 3;
+        } else {
+            throw new NullPointerException();
+        }
+
+        final boolean state = button.isChecked();
+
+        this.mSwitcheroo.flipRelay(index, state, null);
+    }
+
+    /* Activity */
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.control);
+        this.setContentView(R.layout.control);
 
-        getActionBar().setDisplayHomeAsUpEnabled(true);
+        this.getActionBar().setDisplayHomeAsUpEnabled(true);
 
-        mBluetoothDevice = getIntent().getParcelableExtra(ScanActivity.EXTRA_DEVICE);
+        this.mSwitcheroo = this.getIntent().getParcelableExtra(ScanActivity.EXTRA_SWITCHEROO);
+        this.mSwitcheroo.connect(this);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
+        final MenuInflater inflater = this.getMenuInflater();
         inflater.inflate(R.menu.control_menu, menu);
         return true;
     }
 
     @Override
-    public void onStart() {
-        super.onStart();
-
-        mBluetoothGatt = mBluetoothDevice.connectGatt(this, false, mBluetoothGattCallback);
-
-        try {
-            mSemaphore.acquire();
-        } catch(java.lang.InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    BluetoothGattCallback mBluetoothGattCallback = new BluetoothGattCallback() {
-        public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
-            android.util.Log.d(TAG, "onConnectionStateChange");
-
-            if (newState == BluetoothProfile.STATE_CONNECTED) {
-                android.util.Log.d(TAG, "onConnectionStateChange -> STATE_CONNECTED");
-                mBluetoothGatt.discoverServices();
-            }
-        }
-
-        public void onServicesDiscovered(BluetoothGatt gatt, int status) {
-            android.util.Log.d(TAG, "onServicesDiscovered");
-
-            if (status == BluetoothGatt.GATT_SUCCESS) {
-                android.util.Log.d(TAG, "onServicesDiscovered -> GATT_SUCCESS");
-                mBluetoothGattService = mBluetoothGatt.getService(SERVICE_UUID);
-                mSemaphore.release();
-            }
-        }
-    };
-
-    @Override
-    public void onResume() {
-        super.onResume();
-    }
-
-    public void onSwitchClick(View view) {
-        SwitchButton button = (SwitchButton) view;
-
-        final BluetoothGattCharacteristic characteristic;
-
-        if (button.getId() == R.id.switch_1) {
-            characteristic = mBluetoothGattService.getCharacteristic(CHARACTERISTIC_21_UUID);
-        } else if (button.getId() == R.id.switch_2) {
-            characteristic = mBluetoothGattService.getCharacteristic(CHARACTERISTIC_22_UUID);
-        } else if (button.getId() == R.id.switch_3) {
-            characteristic = mBluetoothGattService.getCharacteristic(CHARACTERISTIC_23_UUID);
-        } else if (button.getId() == R.id.switch_4) {
-            characteristic = mBluetoothGattService.getCharacteristic(CHARACTERISTIC_24_UUID);
-        } else {
-            throw new RuntimeException();
-        }
-
-        if (button.isChecked()) {
-            characteristic.setValue(new byte[] { (byte) 0x01, (byte) 0x00 });
-        } else {
-            characteristic.setValue(new byte[] { (byte) 0x00, (byte) 0x00 });
-        }
-
-        mBluetoothGatt.writeCharacteristic(characteristic);
-    }
-
-    @Override
     public void onPause() {
         super.onPause();
+
+        if (this.isFinishing()) {
+            this.mSwitcheroo.disconnect();
+        }
+    }
+
+    /* ISwitcherooCallback */
+
+    public void onSwitcherooConnected() {
+    }
+
+    public void onSwitcherooDisconnected() {
+        this.finish();
     }
 
 }
