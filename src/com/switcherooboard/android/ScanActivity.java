678acd647ecc062c74a2f233a54e7f071e63610a
Drew Carey Buglione <drew@dangerousthings.com>
2015-05-15 23:01:45 -0400

    Redo ScanActivity.java indentation

--- src/com/switcherooboard/android/ScanActivity.java
+++ src/com/switcherooboard/android/ScanActivity.java
@@ -13,72 +13,72 @@ import android.widget.GridView;
 
 public class ScanActivity extends Activity implements BluetoothAdapter.LeScanCallback {
 
-    private static final String TAG = "Switcheroo.ScanActivity";
+  private static final String TAG = "Switcheroo.ScanActivity";
 
-    public static final String EXTRA_SWITCHEROO = "com.switcherooboard.android.extra.SWITCHEROO";
+  public static final String EXTRA_SWITCHEROO = "com.switcherooboard.android.extra.SWITCHEROO";
 
-    BluetoothAdapter mBluetoothAdapter;
+  BluetoothAdapter mBluetoothAdapter;
 
-    GridView mGridView;
+  GridView mGridView;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.scan);
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.scan);
 
-        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-    }
+    mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+  }
 
-    @Override
-    public void onStart() {
-        super.onStart();
+  @Override
+  public void onStart() {
+    super.onStart();
 
-        mGridView = (GridView) findViewById(R.id.gridview);
+    mGridView = (GridView) findViewById(R.id.gridview);
 
-        mGridView.setAdapter(new ArrayAdapter(this, android.R.layout.simple_list_item_1));
+    mGridView.setAdapter(new ArrayAdapter(this, android.R.layout.simple_list_item_1));
 
-        mGridView.setOnItemClickListener(new OnItemClickListener() {
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent = new Intent(ScanActivity.this, MainActivity.class);
+    mGridView.setOnItemClickListener(new OnItemClickListener() {
+      public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Intent intent = new Intent(ScanActivity.this, MainActivity.class);
 
-                final BluetoothDevice device = (BluetoothDevice) parent.getAdapter().getItem(position);
-                intent.putExtra(ScanActivity.EXTRA_SWITCHEROO, new GattSwitcheroo(device.getAddress()));
+        final BluetoothDevice device = (BluetoothDevice) parent.getAdapter().getItem(position);
+        intent.putExtra(ScanActivity.EXTRA_SWITCHEROO, new GattSwitcheroo(device.getAddress()));
 
-                startActivity(intent);
-            }
-        });
-    }
+        startActivity(intent);
+      }
+    });
+  }
 
-    @Override
-    public void onResume() {
-        super.onResume();
+  @Override
+  public void onResume() {
+    super.onResume();
 
-        android.util.Log.d(TAG, "startLeScan()");
-        mBluetoothAdapter.startLeScan(this);
-    }
+    android.util.Log.d(TAG, "startLeScan()");
+    mBluetoothAdapter.startLeScan(this);
+  }
 
-    @Override
-    public void onLeScan(final BluetoothDevice device, int rssi, byte[] scanRecord) {
-        android.util.Log.d(TAG, "onLeScan" + " -> " + "\"" + device.getName() + "\"");
+  @Override
+  public void onLeScan(final BluetoothDevice device, int rssi, byte[] scanRecord) {
+    android.util.Log.d(TAG, "onLeScan" + " -> " + "\"" + device.getName() + "\"");
 
-        if (device.getName().equals("Switcheroo")) {
-            final ArrayAdapter adapter = (ArrayAdapter) mGridView.getAdapter();
+    if (device.getName().equals("Switcheroo")) {
+      final ArrayAdapter adapter = (ArrayAdapter) mGridView.getAdapter();
 
-            runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    if (adapter.getPosition(device) == -1) adapter.add(device);
-                }
-            });
+      runOnUiThread(new Runnable() {
+        @Override
+        public void run() {
+          if (adapter.getPosition(device) == -1) adapter.add(device);
         }
+      });
     }
+  }
 
-    @Override
-    public void onPause() {
-        super.onPause();
+  @Override
+  public void onPause() {
+    super.onPause();
 
-        android.util.Log.d(TAG, "stopLeScan()");
-        mBluetoothAdapter.stopLeScan(this);
-    }
+    android.util.Log.d(TAG, "stopLeScan()");
+    mBluetoothAdapter.stopLeScan(this);
+  }
 
 }
